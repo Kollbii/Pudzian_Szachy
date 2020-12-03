883187f21811ef436cc78bdc237a8392f9ea8ebd
czujsnn <czajsonino@gmail.com>
2020-12-03 18:53:49 +0100

    templatka

--- chess.py
+++ chess.py
@@ -21,12 +21,14 @@ class Board:
         pass
 
 class Pionek:
-    def __init__(self,pawn,id,n,board):
+    def __init__(self,pawn,id,n,board,dim_board):
         self.cords = []
         self.pawn = pawn
         self.id = id
         self.n = n
         self.board = board
+        self.dim = dim_board
+
 
     def PlaceOnBoard(self):
 
