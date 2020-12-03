39d0deb380288096e3f4bc4a2e9bb6f8786511fc
Kollbi <lkollbek@gmail.com>
2020-12-03 23:02:46 +0100

    Caly pudzianSkoczek dziala i szczesliwie podnosi gabaryty

--- chess.py
+++ chess.py
@@ -2,34 +2,52 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import os
+from colorama import init
+from numpy.ma.core import right_shift
+init()
 np.set_printoptions(threshold=np.inf)
 
+GREEN = '\u001b[32m'
+RED = '\u001b[31m'
+RESET = '\u001b[0m'
+
+global info
+info = ''
+
+################################################
+# Ogólnie to w info chcemy zwracać wszystkie informacje więc wystarczy, że każda metoda będzie do niej dodawać informacje wg jakiejś
+# templatki. Kolorki bo #fancy_wygląd i Jemioło się podoba XD <Trzeba uważać na warunki sprawdzania bo to są całe teksy znaków jak np.
+# \u001b[32m♝\u001b[0m zamiast samego ♝. 
+# W sumie zrobiłem tyle, że skoczek sprawdza każdą figurę i ją markuje na czerwono.
+# Jedyne co chyba trzeba zrobić to napisac metody dla hetmana i gońca.
+# Tylko wywołując funkcje trzeba mieć na uwadze kolejność. bo np. Jeśli moje skoczki będą pierwsze to pozamieniają
+# niektóre figury na czerwone. Więc fajnie jakby ten problem jakoś rozwiązać. (albo zrezygnować z kolorwania :(( *sad noises*)
+# Podczas wstępnego testowania nie zauważyłem błędów w działaniu mojej części więc powinno być wszystko gites.
+# Każdy skoczek sprawdza wszystkie możliwe pola dookoła siebie 
+# ~~Kollbi
+#
+#
+################################################
 
 class Board:
     def __init__(self):
         pass
 
-    def createBoard(self,dim): #tworzenie macierzy nxn wypelnionej zerami
+    def createBoard(self, dim): #tworzenie macierzy nxn wypelnionej kropkami <3  
         self.dim = dim
-        self.board = np.zeros((self.dim,self.dim),dtype= "object")    
+        self.board = np.array([[ '•' for x in range(self.dim)] for y in range (self.dim)], dtype="object")
         return self.board
 
-    def writeToFile(self):
-        np.savetxt("board1.txt",self.board, fmt='%s', delimiter=' ')
-
-    def PlaceOnBoard(self,pawn,id,n): #pawn to rodzaj, id to jego litera, n to ilość
-        pass
 
 class Pionek:
-    def __init__(self,pawn,n,board,dim_board):
+    def __init__(self, pawn, n, board, dim_board):
         self.cords = []
         self.pawn = pawn 
         self.n = n
         self.board = board
         self.dim = dim_board
         
-
-    def PlaceOnBoard(self,board):
+    def placeOnBoard(self, board):
 
         for _ in range(self.n):
              
@@ -41,6 +59,7 @@ class Pionek:
                     new_cord = (x,y)
                     if new_cord not in self.cords:
                         self.cords.append(new_cord)
+                        break
             else:
                 self.cords.append(new_cord)
             
@@ -49,26 +68,94 @@ class Pionek:
     
         return board
 
+    def drawTab(self, board):
+        draw = f''
+        for i in range (0, len(self.board)):
+            draw += f"{str(i).rjust(2)}│"
+            for j in range (0, len(board[i])):
+                if(j == len(board[i]) - 1):
+                    draw += ''.join(str(board[i][j]))
+                    draw += ''.join("\n")
+                else:
+                    draw += ''.join(str(board[i][j]))
+                    draw += ''.join(" ") # •
+        return draw
+
 class Hetman(Pionek):
+    #TODO
     pass
 
 class Goniec(Pionek):
-    def __init__(self):
-        print("siemka siemka")
-        pass
-    
-
-class Skoczek(Pionek):
+    #TODO
     pass
 
+class Skoczek(Pionek):
+    def doesCheckKnight(self, board, i, j, x, y):
+        if board[i + x][j + y] == f'{GREEN}♞{RESET}':
+            return True
+        return False
 
-p = os.getcwd()
-if os.path.exists(p+"/board.txt") == True:
-    os.remove(p+"/board.txt")
+    def doesCheckQueen(self, board, i, j, x, y):
+        if board[i + x][j + y] == f'{GREEN}♛{RESET}':
+            return True
+        return False
 
-b = Board()
-tablica = b.createBoard(10)
-h = Hetman("H",5,tablica,b.dim)
-print(tablica)
-print(b.dim)
-print(h.PlaceOnBoard(tablica))
+    def doesCheckBishop(self, board, i, j, x, y):
+        if board[i + x][j + y] == f'{GREEN}♝{RESET}':
+            return True
+        return False
+
+    def markChecked(self, board, i, j, x, y, option):
+        global info
+        board[i][j] = f'{RED}♞{RESET}'
+        board[i + x][j + y] = f'{RED}{option}{RESET}'
+        info += f"♞ na [{i}][{j}]\t szachuje {option} na [{i + x}][{j + y}]\n"
+
+    def isSafe(self, board, i, j, x, y):
+        if (i + x >= 0 and i + x < len(self.board)) and (j + y >= 0 and j + y < len(self.board)):
+            return True
+        return False
+
+    def getChecked(self, board):
+        global info
+        moves = [[1,2],[2,1],[-1,2],[-2,1],[-1,-2],[-2,-1],[1,-2],[2,-1]]
+
+        for i in range (0, len(self.board)):
+            for j in range (0, len(self.board[i])):
+                if self.board[i][j] == f'{GREEN}♞{RESET}' or self.board[i][j] == f'{RED}♞{RESET}':    # Tutaj wybiera też czerwone skoczki bo czerwony może szachować jakiegoś hetmana jeszcze.
+                    for x, y in moves:                                                                  # duplikaty się NIE robią bo w sprawdzaniu jest ustawiony "zielony skoczek" :)
+                        if self.isSafe(board, i, j, x, y):
+                            if self.doesCheckKnight(board, i, j, x, y) == True:
+                                self.markChecked(board, i, j, x, y, "♞")
+                            elif self.doesCheckQueen(board, i, j, x, y) == True:
+                                self.markChecked(board, i, j, x, y, "♛")
+                            elif self.doesCheckBishop(board, i, j, x, y) == True:
+                                self.markChecked(board, i, j, x, y, "♝")
+        return board
+
+# Tutaj tego śmiesznego __init__ = '__name__' wstawić ?
+# wstawiłem ale idk jak to ma działać dokładnie X"D 
+def start():
+    p = os.getcwd()
+    if os.path.exists(p+"/board.txt") == True:
+        os.remove(p+"/board.txt")
+
+    b = Board()
+    tablica = b.createBoard(10)
+    h = Hetman(f"{GREEN}♛{RESET}",2,tablica,b.dim)
+    s = Skoczek(f"{GREEN}♞{RESET}",3,tablica,b.dim)
+    g = Goniec(f"{GREEN}♝{RESET}",2,tablica,b.dim)
+    hetmanPlacement = h.placeOnBoard(tablica)
+    knightPlacement = s.placeOnBoard(hetmanPlacement)
+    bishopPlacement = g.placeOnBoard(knightPlacement)
+
+    test1 = s.getChecked(bishopPlacement)
+    print(s.drawTab(test1))
+    print(info)
+
+if __name__ == "__main__":
+    start()
+
+
+# Test Cases 
+#TODO
